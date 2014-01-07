e4bcb2c49b278a8a96caa147ea8b27fd8a52e85f
Alex Willemsma <alexwillemsma@Alexs-MacBook-Pro-2.local>
2014-01-07 13:27:03 -1000

    Reverting db cleaner script from cypher back to Gremlin.
    I realized that gremlin (and more) was borked because neoid doesn't support neo4j
    2.0 yet, and I was trying to run it against that. Downgrading for now as
    we want proper support.

--- lib/neoid/database_cleaner.rb
+++ lib/neoid/database_cleaner.rb
@@ -1,10 +1,11 @@
 module Neoid
   class NeoDatabaseCleaner
     def self.clean_db(instance, start_node = nil)
-      #start_node ||= instance.db.get_root
-      instance.db.execute_query <<-CYPHER
-        START n0=node(0),nx=node(*) OPTIONAL MATCH n0-[r0]-(),nx-[rx]-() WHERE nx <> n0 DELETE r0,rx,nx
-      CYPHER
+      start_node ||= instance.db.get_root
+      instance.db.execute_script <<-GREMLIN
+        g.V.toList().each { if (it.id != 0) g.removeVertex(it) }
+        g.indices.each { g.dropIndex(it.indexName); }
+      GREMLIN
 
       true
     end
