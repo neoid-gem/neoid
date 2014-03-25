747379a099aad88459bcfe36e051ff31b4614721
minsikzzang <minsikzzang@gmail.com>
2014-03-25 13:26:44 +0000

    removed 'start_node = Neoid.db.get_root' in NeodatabaseCleaner because latest neography get_root function is broken, and NeodatabaseCleaner.clean_db() does not need start_node attribute'

--- lib/neoid/database_cleaner.rb
+++ lib/neoid/database_cleaner.rb
@@ -1,6 +1,6 @@
 module Neoid
   class NeoDatabaseCleaner
-    def self.clean_db(start_node = Neoid.db.get_root)
+    def self.clean_db()
       Neoid.db.execute_script <<-GREMLIN
         g.V.toList().each { if (it.id != 0) g.removeVertex(it) }
         g.indices.each { g.dropIndex(it.indexName); }
