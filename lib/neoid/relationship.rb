f515cb4c662781ce89567a616b10643032201340
Ben A Morgan <ben@benmorgan.io>
2014-06-17 20:30:02 -0700

    clean up relationship.rb

--- lib/neoid/relationship.rb
+++ lib/neoid/relationship.rb
@@ -1,5 +1,71 @@
 module Neoid
   module Relationship
+    class << self
+      def from_hash(hash)
+        relationship = RelationshipLazyProxy.new(hash)
+
+        relationship
+      end
+
+      def included(receiver)
+        receiver.send :include, Neoid::ModelAdditions
+        receiver.send :include, InstanceMethods
+        receiver.extend         ClassMethods
+        
+        initialize_relationship receiver if Neoid.env_loaded
+
+        Neoid.relationship_models << receiver
+      end
+
+      def meta_data
+        @meta_data ||= {}
+      end
+
+      def initialize_relationship(rel_model)
+        rel_model.reflect_on_all_associations(:belongs_to).each do |belongs_to|
+          return if belongs_to.options[:polymorphic]
+
+          # e.g. all has_many on User class
+          all_has_many = belongs_to.klass.reflect_on_all_associations(:has_many)
+
+          # has_many (without through) on the side of the relationship that removes a relationship. e.g. User has_many :likes
+          this_has_many = all_has_many.find { |o| o.klass == rel_model }
+          next unless this_has_many
+
+          # e.g. User has_many :likes, after_remove: ...
+          full_callback_name = "after_remove_for_#{this_has_many.name}"
+          belongs_to.klass.send(full_callback_name) << :neo_after_relationship_remove if belongs_to.klass.method_defined?(full_callback_name)
+
+          # has_many (with through) on the side of the relationship that removes a relationship. e.g. User has_many :movies, through :likes
+          many_to_many = all_has_many.find { |o| o.options[:through] == this_has_many.name }
+          next unless many_to_many
+
+          return if many_to_many.options[:as] # polymorphic are not supported here yet
+
+          # user_id
+          foreign_key_of_owner = many_to_many.through_reflection.foreign_key
+
+          # movie_id
+          foreign_key_of_record = many_to_many.source_reflection.foreign_key
+
+          (Neoid::Relationship.meta_data ||= {}).tap do |data|
+            (data[belongs_to.klass.name.to_s] ||= {}).tap do |model_data|
+              model_data[many_to_many.klass.name.to_s] = [rel_model.name.to_s, foreign_key_of_owner, foreign_key_of_record]
+            end
+          end
+
+          # e.g. User has_many :movies, through: :likes, before_remove: ...
+          full_callback_name = "before_remove_for_#{many_to_many.name}"
+          belongs_to.klass.send(full_callback_name) << :neo_before_relationship_through_remove if belongs_to.klass.method_defined?(full_callback_name)
+
+
+          # e.g. User has_many :movies, through: :likes, after_remove: ...
+          full_callback_name = "after_remove_for_#{many_to_many.name}"
+          belongs_to.klass.send(full_callback_name) << :neo_after_relationship_through_remove if belongs_to.klass.method_defined?(full_callback_name)
+        end
+      end
+    end
+
     # this is a proxy that delays loading of start_node and end_node from Neo4j until accessed.
     # the original Neography Relatioship loaded them on initialization
     class RelationshipLazyProxy < ::Neography::Relationship
@@ -11,13 +77,6 @@ module Neoid
         @end_node_from_db ||= @end_node = Neography::Node.load(@end_node, Neoid.db)
       end
     end
-
-    def self.from_hash(hash)
-      relationship = RelationshipLazyProxy.new(hash)
-
-      relationship
-    end
-
 
     module ClassMethods
       def delete_command
@@ -75,7 +134,7 @@ module Neoid
           relationship
         GREMLIN
 
-         script_vars = {
+        script_vars = {
           unique_id_key: Neoid::UNIQUE_ID_KEY,
           relationship_data: data,
           unique_id: self.neo_unique_id,
@@ -99,64 +158,6 @@ module Neoid
 
       def neo_relationship
         _neo_representation
-      end
-    end
-
-    def self.included(receiver)
-      receiver.send :include, Neoid::ModelAdditions
-      receiver.send :include, InstanceMethods
-      receiver.extend         ClassMethods
-      
-      initialize_relationship receiver if Neoid.env_loaded
-
-      Neoid.relationship_models << receiver
-    end
-
-    def self.meta_data
-      @meta_data ||= {}
-    end
-
-    def self.initialize_relationship(rel_model)
-      rel_model.reflect_on_all_associations(:belongs_to).each do |belongs_to|
-        return if belongs_to.options[:polymorphic]
-
-        # e.g. all has_many on User class
-        all_has_many = belongs_to.klass.reflect_on_all_associations(:has_many)
-
-        # has_many (without through) on the side of the relationship that removes a relationship. e.g. User has_many :likes
-        this_has_many = all_has_many.find { |o| o.klass == rel_model }
-        next unless this_has_many
-
-        # e.g. User has_many :likes, after_remove: ...
-        full_callback_name = "after_remove_for_#{this_has_many.name}"
-        belongs_to.klass.send(full_callback_name) << :neo_after_relationship_remove if belongs_to.klass.method_defined?(full_callback_name)
-
-        # has_many (with through) on the side of the relationship that removes a relationship. e.g. User has_many :movies, through :likes
-        many_to_many = all_has_many.find { |o| o.options[:through] == this_has_many.name }
-        next unless many_to_many
-
-        return if many_to_many.options[:as] # polymorphic are not supported here yet
-
-        # user_id
-        foreign_key_of_owner = many_to_many.through_reflection.foreign_key
-
-        # movie_id
-        foreign_key_of_record = many_to_many.source_reflection.foreign_key
-
-        (Neoid::Relationship.meta_data ||= {}).tap do |data|
-          (data[belongs_to.klass.name.to_s] ||= {}).tap do |model_data|
-            model_data[many_to_many.klass.name.to_s] = [rel_model.name.to_s, foreign_key_of_owner, foreign_key_of_record]
-          end
-        end
-
-        # e.g. User has_many :movies, through: :likes, before_remove: ...
-        full_callback_name = "before_remove_for_#{many_to_many.name}"
-        belongs_to.klass.send(full_callback_name) << :neo_before_relationship_through_remove if belongs_to.klass.method_defined?(full_callback_name)
-
-
-        # e.g. User has_many :movies, through: :likes, after_remove: ...
-        full_callback_name = "after_remove_for_#{many_to_many.name}"
-        belongs_to.klass.send(full_callback_name) << :neo_after_relationship_through_remove if belongs_to.klass.method_defined?(full_callback_name)
       end
     end
   end
